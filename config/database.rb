74306e7ffeea41a5a2fd83770bbef7e02e972c41
Grant Tran <grant.tran@gmail.com>
2020-04-02 07:11:51 +0000

    db

--- config/database.rb
+++ config/database.rb
@@ -4,7 +4,7 @@
       database: "db/db.sqlite3"
     }
   else
-    db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+    db = URI.parse(ENV['DATABASE_URL'] || 'postgres://jzjgqylnfrbucf:462906c98d90831508b096fb36adc99915a7c90d2616290df9b8af23b84fe4cf@ec2-18-235-97-230.compute-1.amazonaws.com:5432/ddu6r2l22ikt6h')
     set :database, {
       adapter: "postgresql",
       host: db.host,
